25d8dba701da88584b2fad4d4e5c4772fc0861af
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2023-05-12 13:59:16 -0600

    nextJS initial check in

---
+++ pages/login/index.tsx
@@ -0,0 +1,10 @@
+import Image from 'next/image'
+import styles from './login.module.css'
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      Login
+    </main>
+  )
+}
