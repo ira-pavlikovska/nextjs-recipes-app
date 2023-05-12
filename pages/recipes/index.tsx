3acfb2aa5f2080fbc24f002491b053faf090a0bd
Iryna Pavlikovska <irina.pavlikovskaya@gmail.com>
2023-05-12 14:16:25 -0600

    add axios

--- pages/recipes/index.tsx
+++ pages/recipes/index.tsx
@@ -1,10 +1,26 @@
 import Image from 'next/image'
 import styles from './recipes.module.css'
+import { RecipeType } from '../../types'
+import {useEffect, useState} from "react";
+import axios from 'axios'
 
 export default function Home() {
+
+  const [recipes, setRecipes] = useState<RecipeType[]>([])
+
+  // react native accepts calls to HTTPS APIs only, so populate data from file as fallback
+  useEffect(() => {
+    axios
+      .get('http://localhost:3000/api/recipes')
+      .then((response: any) => setRecipes(response.data))
+      .catch((error: any) => console.log(error)
+      )
+  }, [])
+
   return (
     <main className={styles.main}>
       Recipes
+      {recipes.map((item: RecipeType, index) => (<div key={index}>{item.recipeName}</div>))}
     </main>
   )
 }
